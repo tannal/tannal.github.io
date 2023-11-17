7e5b9fca80e72d6f1954015db46583c11a0de020
tannal <tannal2409@gmail.com>
2023-11-17 09:43:11 +0800

    site map

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Random Things By Tannal",
+	url: "https://tannal.github.io/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I type like a monkey.You know, I can make great things.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "tannal",
+		email: "tannal2409@gmail.com",
+		url: "https://tannal.github.io/"
 	}
 }
