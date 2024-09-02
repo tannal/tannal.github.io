16190eaab44b76f10d5bb27e5ba0dfc2508d0ebb
tannal <tannal2409@gmail.com>
2024-09-02 17:18:14 +0800

    udpate

--- static/test.js
+++ static/test.js
@@ -1,1 +1,20 @@
-console.log('This works')
+console.log("This works");
+
+// webcompoent counter
+class Counter extends HTMLElement {
+  constructor() {
+    super();
+    this.count = 0;
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+      <button id="increment">Increment</button>
+      <span>${this.count}</span>
+    `;
+    this.shadowRoot
+      .getElementById("increment")
+      .addEventListener("click", () => {
+        this.count++;
+        this.shadowRoot.querySelector("span").textContent = this.count;
+      });
+  }
+}
